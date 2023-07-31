16da782cc0b206b53bf5b3202ecfdf7701ae4c9b
sanjeevm15 <140108723+sanjeevm15@users.noreply.github.com>
2023-07-31 14:25:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package com.first;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World new ");
+        System.out.println("Hello World new one");
     }
 }
